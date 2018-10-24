abd930fdeb2102260648e9b0d58bce99080ad6c0
Peitian Pan <pp482@cornell.edu>
2018-10-24 17:34:35 -0400

    [translation-import] Minor format and bug fix 1. Fixed the spacing issues with head comments 2. Fixed the bug where the hypothesis strategy always generates 9 inports and 8 outports.

--- pymtl/passes/SystemVerilogTranslationPass.py
+++ pymtl/passes/SystemVerilogTranslationPass.py
@@ -4,9 +4,8 @@
 # This pass takes the top module of a PyMTL component
 # and translates it into SystemVerilog.
 #
-# Author          : Shunning Jiang
-# Refactored by   : Peitian Pan
-# Date            : Oct 18, 2018
+# Author : Shunning Jiang, Peitian Pan
+# Date   : Oct 18, 2018
 
 from pymtl                import *
 from pymtl.model          import ComponentLevel1
